0dd098a404f32898ee39e43e9d402d3c485016ab
Aleksandar Bedjovski <aleksandar.bedjovski@internetbrands.com>
2024-05-08 15:57:49 +0200

    R

--- shop/admin.py
+++ shop/admin.py
@@ -160,7 +160,7 @@ class ProductAdmin(admin.ModelAdmin):
                 )
 
                 sendgrid_client.send_mail(
-                    "pevkjevv@gmail.com",
+                    "lilanikolova@yahoo.com",
                     f"{obj.get_product_title_for_accountant_invoice()} - Промена на цена",
                     "<strong>Во attachment</strong>",
                     pdf,
@@ -182,7 +182,7 @@ class ProductAdmin(admin.ModelAdmin):
                         )
                     )
                     sendgrid_client.send_mail(
-                        "pevkjevv@gmail.com",
+                        "lilanikolova@yahoo.com",
                         f"{instance.get_product_title_for_accountant_invoice()} - Промена на цена",
                         "<strong>Во attachment</strong>",
                         pdf,
